a8b792f906c94e25c3e552186a401895ceca359c
remicks <lost.junglist@gmail.com>
2015-02-17 18:38:11 -0600

    Add KToonsez apps
    KTweaker & KMonitor

--- bliss.mk
+++ bliss.mk
@@ -6,3 +6,8 @@ $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := bliss_klte
+
+# KToonsez apps
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/kt/com.ktoonsez.KTmonitor.apk:system/app/com.ktoonsez.KTmonitor.apk \
+    $(LOCAL_PATH)/kt/com.ktoonsez.KTweaker.apk:system/app/com.ktoonsez.KTweaker.apk
